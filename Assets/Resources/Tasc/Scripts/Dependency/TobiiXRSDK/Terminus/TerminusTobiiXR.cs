a2cbb78591b8050a2c7ac9c7432a5075066d95f3
Jinki Jung <hypnotize@kaist.ac.kr>
2020-04-20 01:19:14 +0200

    TobiiXR support - not tested yet

---
+++ Assets/Resources/Tasc/Scripts/Dependency/TobiiXRSDK/Terminus/TerminusTobiiXR.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Tobii.XR;
+
+namespace TascUnity
+{
+    public class TerminusTobiiXR : Terminus
+    {
+
+        void Start()
+        {
+            var settings = new TobiiXR_Settings();
+            TobiiXR.Start(settings);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var eyeTrackingData = TobiiXR.GetEyeTrackingData(TobiiXR_TrackingSpace.World);
+
+            if (eyeTrackingData.GazeRay.IsValid)
+            {
+                var rayOrigin = eyeTrackingData.GazeRay.Origin;
+                SingleConditionPublisher.Instance.Send(new VectorVariableState(this, "GazeOrigin", rayOrigin));
+                var rayDirection = eyeTrackingData.GazeRay.Direction;
+                Quaternion rayDirectionQuat = Quaternion.Euler(rayDirection);
+                SingleConditionPublisher.Instance.Send(new QuaternionVariableState(this, "GazeDirection", rayDirectionQuat));
+            }
+        }
+    }
+}
